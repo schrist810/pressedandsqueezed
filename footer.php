1b1e72a5eda9a20defe942840b3693610c5515d6
Sarah Christ <schrist810@gmail.com>
2015-02-14 10:35:02 -0600

    Footer edit
    added contact info

--- footer.php
+++ footer.php
@@ -11,11 +11,11 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'pressedandsqueezed' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'pressedandsqueezed' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'pressedandsqueezed' ), 'pressedandsqueezed', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
+		<footer>
+	<h1>Contact Us</h1>
+	<p>Pressed and Squeezed</p>
+	<p>2525 Orange Way, Orchard, CA 91104</p>
+	<p>Phone: (800) 424-3232</p>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
