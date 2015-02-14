e5f6f7a59194c867dff43086d818474c39b75fa9
Sarah Christ <schrist810@gmail.com>
2015-02-14 11:19:37 -0600

    Clearfix adjust

--- header.php
+++ header.php
@@ -9,15 +9,16 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<title><?php bloginfo('name'); ?></title>
-		<script src="http://use.edgefonts.net/josefin-slab.js"></script>
-		<script src="http://use.edgefonts.net/merriweather.js"></script>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site clearfix">
+<div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'pressedandsqueezed' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
