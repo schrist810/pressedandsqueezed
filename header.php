5f91e26f7d96391d6896cb262912a93f5ccf2a9c
Sarah Christ <schrist810@gmail.com>
2015-02-14 11:12:57 -0600

    Footer styling

--- header.php
+++ header.php
@@ -9,16 +9,15 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<title><?php bloginfo('name'); ?></title>
+		<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+		<script src="http://use.edgefonts.net/merriweather.js"></script>
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<div id="page" class="hfeed site clearfix">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'pressedandsqueezed' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
